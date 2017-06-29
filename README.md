e9200cf3c23b119a8898ce4964f496e9619efa64
egin10 <egin066@gmail.com>
2017-06-29 11:36:05 +0800

    add validator

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # session_login_basic
-Learn about login using express-session
+Learn about login using express-session & express-validator
+
+use :
+---
+1. clone repository : `git clone https://github.com/egin10/session_login_basic.git`
+2. goto repository : `cd session_login_basic && npm i`
+3. run it use : `node app` or `node app.js`
+4. server will running on `http://localhost:8000/`
+
+enjoy it!
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var path = require('path');
 var swig = require('swig');
 var bodyParser = require('body-parser');
 var expressSession = require('express-session');
+var validator = require('express-validator');
 
 var app = express();
 
@@ -16,11 +18,12 @@ app.use(expressSession({
   resave: false,
   saveUninitialized: false
 }));
+app.use(validator());
 
 var sess; //variable for store session
 app.get('/', function(req, res){
   sess = req.session;
-  if(sess.username == 'admin'){
+  if(sess.email == 'egin@example.com'){
     res.redirect('/admin');
   } else {
     res.render('login');
@@ -28,20 +31,26 @@ app.get('/', function(req, res){
 });
 
 app.post('/login', function(req, res){
-  sess = req.session;
-  if(req.body.username == 'admin' && req.body.password == 'admin'){
-    sess.username = req.body.username;
-    sess.password = req.body.password;
-    res.redirect('/admin');
+  req.checkBody('email', 'Email is Invalid!').isEmail();
+  var errors = req.validationErrors();
+  if(errors){
+    res.render('login', { errors: errors });
+    return;
   } else {
-    res.redirect('/')
+    sess = req.session;
+    if(req.body.email == 'egin@example.com' && req.body.password == 'admin'){
+      sess.email = req.body.email;
+      res.redirect('/admin');
+    } else {
+      res.redirect('/');
+    }
   }
 });
 
 app.get('/admin', function(req, res){
   sess = req.session;
-  if(sess.username == 'admin'){
-    res.render('index', { username : sess.username });
+  if(sess.email == 'egin@example.com'){
+    res.render('index', { email : sess.email });
   } else {
     res.send("You're not yet Login! <a href='/'>Log In</a>");
   }
@@ -55,6 +64,11 @@ app.get('/logout', function(req, res){
       res.redirect('/');
     }
   });
+});
+
+app.get('*', function(req, res){
+  res.writeHead(404, {"Content-Type":"text/plain"});
+  res.end('Page Not Found!');
 });
 
 //============= LISTEN SERVER
